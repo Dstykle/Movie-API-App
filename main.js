cd79e47fb8c87e448d0c057a11a4c256cadde9ca
Dstykle <100395355+Dstykle@users.noreply.github.com>
2025-07-22 15:34:00 -0400

    visual update

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 async function searchMovie() {
   const title = document.getElementById('movieTitle').value;
-  const apiKey = 'YOUR_OMDB_API_KEY'; // Replace with your real key
 
   const url = `https://www.omdbapi.com/?i=tt3896198&apikey=378ecac&t=${encodeURIComponent(title)}`;
 
@@ -10,13 +9,16 @@ async function searchMovie() {
 
     if (data.Response === "True") {
       document.getElementById('output').innerHTML = `
-        <h2>${data.Title}</h2>
+        <h2 style="font-size:4vw">${data.Title}</h2>
         <img src="${data.Poster}" alt="Movie Poster" style="max-width:200px;">
-        <p class="detail" style=""><strong>Genre</strong>: ${data.Genre}\n\n<strong>Year</strong>: ${data.Year}\n\n<strong>Directors:</strong> ${data.Director}\n\n<strong>Actors:</strong> ${data.Actors}\n\n<strong>Summary:</strong> ${data.Plot}\n\n\n</p><p><strong>Ratings:</strong></p>`;
+        <p class="detail"><strong>Genre</strong>: ${data.Genre}\n\n<strong>Year</strong>: ${data.Year}\n\n<strong>Directors:</strong> ${data.Director}\n\n<strong>Actors:</strong> ${data.Actors}\n\n<strong>Summary:</strong> ${data.Plot}\n\n\n</p><p><strong>Ratings:</strong></p>`;
       for(let i = 0; i < data.Ratings.length;i++){
               const para = document.createElement("p");
+        para.classList.add("size");
+
               const para1 = document.createElement("p");
-        const node = document.createTextNode("By: "+ data.Ratings[i]['Source']);
+        para1.classList.add("list");
+              const node = document.createTextNode("By: "+ data.Ratings[i]['Source']);
         const node1 = document.createTextNode("Rating: "+ data.Ratings[i]['Value']); 
 
         para.appendChild(node);
@@ -32,17 +34,3 @@ async function searchMovie() {
     console.error(error);
   }
 }
-
-    /*
-  try {
-    const response = await fetch(url);
-    const data = await response.json();
-    document.getElementById('output').textContent = JSON.stringify(data, null, 2);
-  } catch (error) {
-    console.error('Error fetching movie data:', error);
-    document.getElementById('output').textContent = 'Error fetching data.';
-  }
-}
-
-
-http://www.omdbapi.com/?i=tt3896198&apikey=378ecac&t=${encodeURIComponent(title)}*/
